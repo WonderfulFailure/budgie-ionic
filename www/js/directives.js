004f0e35b58a85fc058b7e29d7d588219e62ddd4
Devin Lumley <keiroshin@gmail.com>
2015-04-12 22:03:16 -0700

    Improving reliability of autofocus

--- www/js/directives.js
+++ www/js/directives.js
@@ -1,13 +1,17 @@
 angular.module('budgie.directives', ['ngStorage'])
 
 .directive('focusMe', function($timeout) {
+  var timeout;
   return {
     link: function(scope, element, attrs) {
       scope.$watch(attrs.focusMe, function(value) {
         if(value === true) {
-          $timeout(function() {
-            element[0].focus();
-            scope[attrs.focusMe] = false;
+          if(timeout) clearTimeout(timeout);
+          timeout = setTimeout(function() {
+            if(scope[attrs.focusMe] === true) {
+              element[0].focus();
+              scope[attrs.focusMe] = false;
+            }
           }, 350);
         }
       });
